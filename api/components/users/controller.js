50a7d401baf4c136882c9ae3c74946a4b2dfd905
Ben-init <lopezpetroni@outlook.com>
2022-10-15 20:02:08 -0300

    [update] controller function with params

--- api/components/users/controller.js
+++ api/components/users/controller.js
@@ -1,11 +1,29 @@
-const store = require('../../../store/dummy');
 
-const TABLE = 'user';
 
-function list() {
-    return store.list(TABLE);
-}
+module.exports = function(injectedStore) {
+    const store = injectedStore || require('./../../../store/dummy');
+    const TABLE = 'user';
+    
+    function list() {
+        return store.list(TABLE);
+    }
+    
+    function get(id) {
+        return store.get(TABLE, id);
+    }
+    
+    function add(data) {
+        return store.upsert(TABLE, data)
+    }
 
-module.exports = {
-    list,
+    function remove(id) {
+        return store.remove(TABLE, id);
+    }
+
+    return {
+        list,
+        get,
+        add,
+        remove
+    };
 }
