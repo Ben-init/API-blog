abe5b53b6aa5d38d72d359b6b329cbf940ff27fd
Ben-init <lopezpetroni@outlook.com>
2022-10-17 13:56:17 -0300

    [add] docstrings

--- network/response.js
+++ network/response.js
@@ -1,3 +1,11 @@
+/**
+ * Sends a response with the given message and status.           
+ * @param {Request} req - the request object           
+ * @param {Response} res - the response object           
+ * @param {string} message - the message to send           
+ * @param {number} [status=200] - the status code to send           
+ * @returns None           
+ */
 const success = (req, res, message, status = 200) => {
     res.status(status).send({
         error: false,
@@ -5,6 +13,14 @@ const success = (req, res, message, status = 200) => {
     });
 };
 
+/**
+ * Sends an error response to the client.           
+ * @param {Request} req - The request object.           
+ * @param {Response} res - The response object.           
+ * @param {string} message - The error message.           
+ * @param {number} [status=500] - The status code to send.           
+ * @returns None           
+ */
 const error = (req, res, message, status = 500) => {
     const errorMessage = message || 'Internal server error';
 
