82a5b2184c70cc5e40112df12df278f0f304d370
Ben-init <lopezpetroni@outlook.com>
2022-10-17 13:57:56 -0300

    [update] 'async await' to functions

--- api/components/users/network.js
+++ api/components/users/network.js
@@ -4,15 +4,43 @@ const router = express.Router();
 const response = require('./../../../network/response');
 const controller = require('./index');
 
-router.get('/', (req, res) => {
-    const list = controller.list();
-    response.success(req, res, list);
+router.get('/', async (req, res) => {
+    try {
+        const list = await controller.list();
+        response.success(req, res, list);
+    } catch (err) {
+        response.error(req, res, err);
+    }
 });
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params
-    const list = controller.get(id);
-    response.success(req, res, list);
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const list = await controller.get(id);
+        response.success(req, res, list);
+    } catch (err) {
+        response.error(req, res, err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const data = req.body;
+        const list = await controller.add(data);
+        response.success(req, res, list);
+    } catch (err) {
+        response.error(req, res, err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const list = await controller.remove(id);
+        response.success(req, res, list);
+    } catch (err) {
+        response.error(req, res, err);
+    }
 });
 
 module.exports = router;
