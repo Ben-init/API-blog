30138866597ff7766e538fcab044e30680c2599e
Ben-init <lopezpetroni@outlook.com>
2022-10-17 20:33:36 -0300

    [add] bcrypt hash

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,13 +1,15 @@
+const bcrypt = require('bcrypt');
 const auth = require('../../../auth');
 
 module.exports = function(injectedStore) {
     const store = injectedStore || require('./../../../store/dummy');
     const TABLE = 'auth';
     
-    async function login(username, password) {
+    async function login(username, pass) {
         const data = await store.query(TABLE, { username: username });
-
-        if (data.password !== password) {
+        const verifiedPass = await bcrypt.compare(pass, data.password);
+ 
+        if (!verifiedPass) {
             throw new Error('Invalid data');
         } 
         return auth.sign({
@@ -16,10 +18,11 @@ module.exports = function(injectedStore) {
         });
     }
 
-    function add(data) {
+    async function add(data) {
         const authData = {
             ...data
         }
+        authData.password = await bcrypt.hash(data.password, 8);
 
         return store.add(TABLE, authData);
     }
