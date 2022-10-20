0fc67b7d06281bde8aa6ac0ba42b088aced71138
Ben-init <lopezpetroni@outlook.com>
2022-10-19 22:56:59 -0300

    [add] token functions (unfinished)

--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,33 @@
 const jwt = require('jsonwebtoken');
+const { jwt: jwtConfig } = require('../config');
 
 function sign(data) {
-    return jwt.sign(data, 'secret');
+    return jwt.sign(data, jwtConfig.secret);
+}
+
+function getToken(authorization) {
+    if (!authorization) {
+        throw new Error('token not found');
+    } else if (!authorization.includes('Bearer ')) {
+        throw new Error('invalid token');
+    }
+    const token = authorization.replace('Bearer ', '');
+    
+    return token;
+}
+
+function verify(token) {
+    return jwt.verify(token, jwtConfig.secret)
+}
+
+function decodeHeader(req) {
+    const auth = req.headers.authorization || '';
+    const token = getToken(auth);
+    const decoded = verify(token);
+
+    req.user = decoded;
+
+    return decoded;
 }
 
 module.exports = {
