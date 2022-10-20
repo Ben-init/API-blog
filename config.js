91517b8058ec0da27058ffead3bced6e3fc184d4
Ben-init <lopezpetroni@outlook.com>
2022-10-19 22:57:38 -0300

    [add] jwt secret env

--- config.js
+++ config.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 const config = {
     api: {
         port: process.env.API_PORT || 3000,
-    }
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET,
+    },
 };
 
 module.exports = config;
