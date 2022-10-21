005d3135ac16ef0d4d076be8100a175cd859a3f7
Ben-init <lopezpetroni@outlook.com>
2022-10-21 19:19:12 -0300

    [add] error function

--- api/components/auth/network.js
+++ api/components/auth/network.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const response = require('./../../../network/response');
 const controller = require('./index');
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     try {
         const { username, password } = req.body;
         const login = await controller.login(username, password);
         response.success(req, res, login);
     } catch (err) {
-        response.error(req, res, err);
+        next(err);
     }
 });
 
