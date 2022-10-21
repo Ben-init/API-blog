135e165fae711c2b56c44e38e11c9703c21471f3
Ben-init <lopezpetroni@outlook.com>
2022-10-21 19:24:15 -0300

    [add] error function

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,21 +1,22 @@
 const bcrypt = require('bcrypt');
 const auth = require('../../../auth');
+const error = require('./../../../utils/error');
 
 module.exports = function(injectedStore) {
     const store = injectedStore || require('./../../../store/dummy');
     const TABLE = 'auth';
     
     async function login(username, pass) {
-        const data = await store.query(TABLE, { username: username });
-        const verifiedPass = await bcrypt.compare(pass, data.password);
- 
-        if (!verifiedPass) {
-            throw new Error('Invalid data');
-        } 
-        return auth.sign({
-            sub: data.id,
-            username: data.username,
-        });
+            const data = await store.query(TABLE, { username: username });
+            const verifiedPass = await bcrypt.compare(pass, data.password);
+            
+            if (!verifiedPass) {
+                throw error('Invalid data', 400);
+            } 
+            return auth.sign({
+                sub: data.id,
+                username: data.username,
+            });
     }
 
     async function add(data) {
