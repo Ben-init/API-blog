b6a942bbd74999392e84a06fd41138daa551d669
Ben-init <lopezpetroni@outlook.com>
2022-10-21 19:18:21 -0300

    [add] error function

--- api/index.js
+++ api/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const { api } = require('../config.js');
 const Routes = require('../network/routes');
+const errors = require('./../network/error');
 
 const app = express();
 
 app.use(express.json());
 
 Routes(app);
+
+app.use(errors);
 
 app.listen(api.port);
 
