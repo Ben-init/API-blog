f35cf9c3c9cf54aca352658af5fc51f49dd9e134
Ben-init <lopezpetroni@outlook.com>
2022-10-24 21:12:11 -0300

    [update] update Database (mysql)

--- config.js
+++ config.js
@@ -7,6 +7,12 @@ const config = {
     jwt: {
         secret: process.env.JWT_SECRET,
     },
+    mysql : {
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        pass: process.env.MYSQL_PASS,
+        database: process.env.MYSQL_DB,
+    },
 };
 
 module.exports = config;
---
+++ store/mysql.js
@@ -0,0 +1,72 @@
+const mysql = require('mysql');
+const { promisify } = require('util');
+
+const { mysql : sqlConfig } = require('./../config');
+
+const dbConfig = {
+    host: sqlConfig.host,
+    user: sqlConfig.user,
+    password: sqlConfig.pass,
+    database: sqlConfig.database,
+};
+
+let connection;
+
+function getConnection() {
+    connection = mysql.createConnection(dbConfig);
+
+    connection.connect((error) => {
+        if (error) {
+            console.error('[database error]', error);
+            setTimeout(getConnection, 2000);
+        } else {
+            console.log('DB CONNECTED');
+        }
+    });
+
+    connection.on('error', (error) => {
+        console.error('[database error]', error);
+        if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+            getConnection();
+        } else {
+            throw error;
+        }
+    })
+}
+
+getConnection();
+
+connection.query = promisify(connection.query);
+
+async function list(table) {
+    const dataList = await connection.query(`SELECT * FROM ${table}`);
+    return dataList;
+}
+
+async function get(table, id) {
+    const data = await connection.query(`SELECT * FROM ${table} WHERE id = ?`, [id]);
+    return data;
+}
+
+async function add(table, data) {
+    const newData = await connection.query(`INSERT INTO ${table} SET ?`, data);
+    return newData;
+}
+
+async function update(table, id,data ) {
+    const updatedData = await connection.query(`UPDATE ${table} SET ? WHERE id=?`, [data, id]);
+    return updatedData;
+}
+
+async function query(table, query) {
+    const response = await connection.query(`SELECT * FROM ${table} WHERE ?`, [query]);
+    return response[0];
+}
+
+module.exports = {
+    list,
+    get,
+    add,
+    update,
+    query
+}
