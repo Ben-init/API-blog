7df8a6a1883c1c3769b696318cf71d6bd35e507b
Ben-init <lopezpetroni@outlook.com>
2022-12-02 16:58:10 -0300

    [update] remoteDB verification

--- api/components/auth/index.js
+++ api/components/auth/index.js
@@ -1,4 +1,11 @@
-const store = require('./../../../store/remote-mysql');
+const { remoteDB } = require('./../../../config');
+
+let store = require('./../../../store/mysql');
+
+if (remoteDB) {
+    store = require('./../../../store/remote-mysql');
+}
+
 const controller = require('./controller');
 
 module.exports = controller(store);
