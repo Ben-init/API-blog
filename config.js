3c9e42c324c1a55523e92475a1fc1526073a21e1
Ben-init <lopezpetroni@outlook.com>
2022-12-27 20:42:29 -0300

    [add] cache configs (port,host,user,pass)

--- config.js
+++ config.js
@@ -14,9 +14,19 @@ const config = {
         pass: process.env.MYSQL_PASS,
         database: process.env.MYSQL_DB,
     },
-    mysqlService : {
+    mysqlService: {
         port: process.env.MYSQL_SERVICE_PORT || 3001,
         host: process.env.MYSQL_SERVICE_HOST || 'localhost',
+    },
+    cacheService: {
+        port: process.env.CACHE_SERVICE_PORT || 3003,
+        host: process.env.CACHE_SERVICE_HOST || 'localhost',
+    },
+    redisConfig: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        user: process.env.REDIS_USER,
+        pass: process.env.REDIS_PASS,
     },
     post: {
         port: 3002,
