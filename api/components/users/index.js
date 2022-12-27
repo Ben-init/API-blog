4d0001c92c17bb686ba89c8879d9592df9a9f629
Ben-init <lopezpetroni@outlook.com>
2022-12-27 20:44:45 -0300

    [update] cache remote verification

--- api/components/users/index.js
+++ api/components/users/index.js
@@ -1,11 +1,13 @@
 const { remoteDB } = require('./../../../config');
 
 let store = require('./../../../store/mysql');
+let cache = require('./../../../store/redis');
 
-if (remoteDB) {
+if (remoteDB === true) {
     store = require('./../../../store/remote-mysql');
+    cache = require('./../../../store/remote-cache');
 }
 
 const controller = require('./controller');
 
-module.exports = controller(store);
+module.exports = controller(store, cache);
